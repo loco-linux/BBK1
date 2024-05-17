3abdfaf5514b2c0f36cf41082ff107d1b0e91396
jbarr <jai.barraza@duocuc.cl>
2024-05-17 19:06:12 -0400

    Actividad para POO I. Desarrollo de aplicacion de gestion de cuentas corrientesde Bank Boston. (v2): Se corrigieron lineas de codigo, y se agregaron comentarios. Desarrollado por Jaime Barraza & Sebastian Olave.

--- src/com/duoc/cliente/Cliente.java
+++ src/com/duoc/cliente/Cliente.java
@@ -10,12 +10,15 @@ import java.util.Scanner;
 
 public class Cliente {
 
-    
+    // Atributos encapsulados
+    // Arreglo de constructor del objeto clientes tipo CuentaCorriente
+    // con capacidad para almacenar 10 cuentas (esto fue arbitrario, podría ser otro valor)
     private static final CuentaCorriente[] clientes = new CuentaCorriente[10];
     private static final Scanner teclado = new Scanner(System.in);
     
     private static int contador = 0;
     
+    // Atributos a solicitar al usuario
     private static final String[] caracteristicas = {"Rut",
             "Nombre","Apellido paterno","Apellido materno", 
             "Domicilio", "Comuna", "Telefono"
@@ -25,7 +28,7 @@ public class Cliente {
         
         
         int opcion;
-        boolean salir=false; // continua=false;
+        boolean salir=false; 
         
         do{
         try{
@@ -73,6 +76,10 @@ public class Cliente {
         int len = caracteristicas.length;
         
         for(int i=0; i<len;i++){
+            if(contador == 10){
+                System.out.println("[ERROR] Ya se ocuparon las 10 cuentas");
+                break;
+            }
             System.out.print("Ingrese " + caracteristicas[i] + ": ");
             result[i] = tecla2.nextLine();
             if(caracteristicas[i].equals("Rut")){
@@ -87,6 +94,7 @@ public class Cliente {
             }
         }
         
+        if(contador<10){
         try{
         clientes[contador] = new CuentaCorriente(
                 result[0], // rut
@@ -97,15 +105,16 @@ public class Cliente {
                 result[5], // comuna
                 Integer.parseInt(result[6]), // telefono
                 numeroCuenta(result[0]), // cuenta
-                0 // saldo
+                0 // saldo (ya seteado a $0 pesos)
         );
         
         System.out.println("-> Cuenta Corriente: " + clientes[contador].getCuenta());
         contador++;
+        
         }catch(NumberFormatException io){
             System.out.println("[ERROR]");       
         }
-        
+        }
     
     }
     
@@ -232,26 +241,27 @@ public class Cliente {
         char c_array[] = rut.toCharArray();
         
         char ult4[] = new char[4];
-        ult4[3]= c_array[numDigitos-1];
-        ult4[2]= c_array[numDigitos-3];
-        ult4[1]= c_array[numDigitos-4];
-        ult4[0]= c_array[numDigitos-5];
+        ult4[3]= c_array[numDigitos-3]; // ultimos 4 digitos antes del gion
+        ult4[2]= c_array[numDigitos-4]; // 
+        ult4[1]= c_array[numDigitos-5];
+        ult4[0]= c_array[numDigitos-7];
         
         String ultimos = "";
         for(int i=0; i<4; i++){
             ultimos = ultimos + ult4[i];
         }
-                   
+        // linea que devuelve un numero entero al azar del 0 al 9           
         int random = (int)Math.floor(Math.random()*10);
-        
+        // String que agrupa el prefijo del banco + 4 ultimos digitos del rut + random
+        // Generando los 9 digitos requeridos para la cuenta corriente
         String num = "3343" + ultimos + random;
-        
+        // linea que convierte el tipo String a int
         numeroCuenta = Integer.parseInt(num);
         
         return numeroCuenta;
     }
     
-    
+    // Metodo que comprueba la longitud de caracteres del rut ingresado
     public static boolean comprobarRUT(String rut){
         int numDigitos = rut.trim().length();
       
@@ -259,6 +269,7 @@ public class Cliente {
         
     }
     
+    // Metodo que comprueba si existe el rut ingresado. Si ya esta ingresado, retorna true
     public static boolean existeRUT(String rut){
         boolean existe = false;
         for(int i=0; i<contador; i++){
@@ -271,6 +282,9 @@ public class Cliente {
         return existe;
     }
     
+    // Metodo que comprueba la existencia de una cuenta en particular
+    // Si no existe, retorna -1. 
+    // De lo contrario, retorna el indice del arreglo donde encontro el num de cuenta
     public static int comprobarCuenta(int cuenta){
         boolean existe = false;
         int cont=0;
